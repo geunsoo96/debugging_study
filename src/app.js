dfb3b68524a6f2e86daaa4f9469755d93cb5330e
geunsoo <geunsoo1106@gmail.com>
2022-10-13 18:01:11 +0900

    걍 circle 하나 객체로 만들어봄

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import areaOfCircle from "./modules/areaOfCircle.js";
 
-const getFromDataSomeWhare = 300;
-console.log(areaOfCircle());
+// const getFromDataSomeWhare = 300;
+let width = "300px";
+let height = "300px";
+
+class CreateCircle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+const circle = new CreateCircle(width, height);
+console.log(circle);
+// console.log(areaOfCircle());
